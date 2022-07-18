384e59ac5f3957d72797f9dba42b696f8cc87f72
purple_orange_beach <52859277+smyklaaa@users.noreply.github.com>
2022-07-18 11:29:27 +0200

    stworzenie szkieletu funkci wybierajacej okres  czasu dla ktorego sprawdzamy informacje

--- main.py
+++ main.py
@@ -12,24 +12,31 @@ def main():
     data = response.json()
 
     what_next = input("Wpisz numer danch które chcesz sprawdzić:\n"
-                      "(1)Ile zakażen\n(2)Ile osób zmarło\n(3)Ile ozdrowieńców\n")
+                      "(1)Zakażenia\n(2)Zgony\n(3)Ozdrowieńcy\n")
 
     while what_next in ("1","2","3"):
 
         if what_next == "1":
             os.system("cls")
-            what_next = input("Wpisz numer danch okresu który chcesz sprawdzić:\n"
-                              "(1)Rok\n(2)Miesiac\n(3)Dzien\n(4)Cały okres pandemi\n")
+            what_time = input("Wpisz numer danch okresu który chcesz sprawdzić:\n"
+                              "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
+                              "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
+            period_of_time(what_time)
 
         elif what_next == "2":
             os.system("cls")
-            what_next = input("Wpisz numer danch które chcesz sprawdzić:\n"
-                              "(1)Rok\n(2)Miesiac\n(3)Dzien\n(4)Cały okres pandemi\n")
+            what_time = input("Wpisz numer danch które chcesz sprawdzić:\n"
+                              "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
+                              "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
+            period_of_time(what_time)
 
         elif what_next == "3":
             os.system("cls")
-            what_next = input("Wpisz numer danch które chcesz sprawdzić:\n"
-                              "(1)Rok\n(2)Miesiac\n(3)Dzien\n(4)Cały okres pandemi\n")
+            what_time = input("Wpisz numer danch które chcesz sprawdzić:\n"
+                              "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
+                              "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
+            period_of_time(what_time)
+
 
 
 def make_list_of_countries():
@@ -41,6 +48,23 @@ def make_list_of_countries():
     for country in data:
         list_of_countries.append(country["Country"])
     return list_of_countries
+
+def period_of_time(what_time, which_data):
+    while what_time in ("1", "2", "3", "4"):
+        if what_time == "1":
+            os.system("cls")
+
+
+        elif what_time == "2":
+            os.system("cls")
+
+
+        elif what_time == "3":
+            os.system("cls")
+
+
+        elif what_time == "4":
+            os.system("cls")
 
 
 
