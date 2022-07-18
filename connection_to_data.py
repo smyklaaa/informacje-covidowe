917e1315710481af35ff1baeb771ab2007a8a528
purple_orange_beach <52859277+smyklaaa@users.noreply.github.com>
2022-07-18 18:18:50 +0200

    wtepnie rozdzielam projekt na klasy

---
+++ connection_to_data.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class ConnectionToData:
+    """klasa słuzacza do pobrania danych """
+
+    def __init__(self, country):
+        self.API_URL_COUNTRY = "https://api.covid19api.com/dayone/country/"
+        self.country = country
+
+    def make_main_data(self):
+        """polaczeni sie z glownymi danymi """
+
+        while self.country not in self.make_list_of_countries():
+            self.country = input("Nie znaleziono kraju, jakie panstwo chcesz sprawdzić? ANG: ")
+        response = requests.get(f"{self.API_URL_COUNTRY}{self.country}")
+        main_data = response.json()
+        return main_data
+
+    def make_list_of_countries(self):
+        """tworzenie listy panstw dostepnych w bazie danych"""
+
+        response = requests.get("https://api.covid19api.com/countries")
+        data = response.json()
+        list_of_countries = []
+        for country in data:
+            list_of_countries.append(country["Country"])
+        return list_of_countries
