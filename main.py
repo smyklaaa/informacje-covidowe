c84a92f7c5acf373ddd54200745f4a28d3ac423e
purple_orange_beach <52859277+smyklaaa@users.noreply.github.com>
2022-07-20 15:09:04 +0200

    dodanie mozliwosci szukania danych o wielu panstwach podczas uruchomienia programu

--- main.py
+++ main.py
@@ -6,81 +6,87 @@ print("Zródło danych: https://covid19api.com/")
 
 
 def main():
-    country = input("Jakie panstwo chcesz sprawdzić? ANG: ")
-    main_data = ConnectionToData(country)
-    main_data = main_data.make_main_data()
-
     while True:
-        what_next = input("Wpisz numer danych które chcesz sprawdzić:\n"
-                          "(1)Zakażenia\n(2)Zgony\n(3)Ozdrowieńcy\n")
+        country = input("Jakie panstwo chcesz sprawdzić? ANG: ")
+        main_data = ConnectionToData(country)
+        main_data = main_data.make_main_data()
 
-        while what_next not in ("1", "2", "3"):
-            print("Niepoprawne zapytanie")
+        while True:
             what_next = input("Wpisz numer danych które chcesz sprawdzić:\n"
                               "(1)Zakażenia\n(2)Zgony\n(3)Ozdrowieńcy\n")
 
-        while what_next in ("1", "2", "3"):
+            while what_next not in ("1", "2", "3"):
+                print("Niepoprawne zapytanie")
+                what_next = input("Wpisz numer danych które chcesz sprawdzić:\n"
+                                  "(1)Zakażenia\n(2)Zgony\n(3)Ozdrowieńcy\n")
 
-            if what_next == "1":
-                os.system("cls")
-                what_time = input("Wpisz numer danych okresu który chcesz sprawdzić:\n"
-                                  "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
-                                  "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
+            while what_next in ("1", "2", "3"):
 
-                while what_time not in ("1", "2", "3", "4"):
-                    print("Niepoprawne zapytanie")
+                if what_next == "1":
+                    os.system("cls")
                     what_time = input("Wpisz numer danych okresu który chcesz sprawdzić:\n"
                                       "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
                                       "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
 
-                what_next = "Confirmed"
+                    while what_time not in ("1", "2", "3", "4"):
+                        print("Niepoprawne zapytanie")
+                        what_time = input("Wpisz numer danych okresu który chcesz sprawdzić:\n"
+                                          "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
+                                          "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
 
-                result = CheckData(what_time, main_data, country, what_next).main()
-                if what_time in ("1", "2", "3"):
-                    print(f"Suma zakażeń w podanym okresie: {result}")
-                break
+                    what_next = "Confirmed"
 
-            elif what_next == "2":
-                os.system("cls")
-                what_time = input("Wpisz numer danych które chcesz sprawdzić:\n"
-                                  "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
-                                  "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
+                    result = CheckData(what_time, main_data, country, what_next).main()
+                    if what_time in ("1", "2", "3"):
+                        print(f"Suma zakażeń w podanym okresie: {result}")
+                    break
 
-                while what_time not in ("1", "2", "3", "4"):
-                    print("Niepoprawne zapytanie")
-                    what_time = input("Wpisz numer danych okresu który chcesz sprawdzić:\n"
+                elif what_next == "2":
+                    os.system("cls")
+                    what_time = input("Wpisz numer danych które chcesz sprawdzić:\n"
                                       "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
                                       "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
 
-                what_next = "Deaths"
-                result = CheckData(what_time, main_data, country, what_next).main()
-                if what_time in ("1", "2", "3"):
-                    print(f"Suma zgonow w podanym okresie: {result}")
-                break
+                    while what_time not in ("1", "2", "3", "4"):
+                        print("Niepoprawne zapytanie")
+                        what_time = input("Wpisz numer danych okresu który chcesz sprawdzić:\n"
+                                          "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
+                                          "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
 
-            elif what_next == "3":
-                os.system("cls")
-                what_time = input("Wpisz numer danych które chcesz sprawdzić:\n"
-                                  "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
-                                  "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
+                    what_next = "Deaths"
+                    result = CheckData(what_time, main_data, country, what_next).main()
+                    if what_time in ("1", "2", "3"):
+                        print(f"Suma zgonow w podanym okresie: {result}")
+                    break
 
-                while what_time not in ("1", "2", "3", "4"):
-                    print("Niepoprawne zapytanie")
-                    what_time = input("Wpisz numer danych okresu który chcesz sprawdzić:\n"
+                elif what_next == "3":
+                    os.system("cls")
+                    what_time = input("Wpisz numer danych które chcesz sprawdzić:\n"
                                       "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
                                       "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
 
-                what_next = "Recovered"
-                result = CheckData(what_time, main_data, country, what_next).main()
-                if what_time in ("1", "2", "3"):
-                    print(f"Suma osób które wyzdrowiały w podanym okresie: {result}")
+                    while what_time not in ("1", "2", "3", "4"):
+                        print("Niepoprawne zapytanie")
+                        what_time = input("Wpisz numer danych okresu który chcesz sprawdzić:\n"
+                                          "(1)Suma w ciagu roku\n(2)Suma w ciągu miesiaca\n"
+                                          "(3)Suma w ciągu dnia\n(4)Cały okres pandemi\n")
+
+                    what_next = "Recovered"
+                    result = CheckData(what_time, main_data, country, what_next).main()
+                    if what_time in ("1", "2", "3"):
+                        print(f"Suma osób które wyzdrowiały w podanym okresie: {result}")
+                    break
+
+            go_on = input("Czy chcesz kontynuowac sprawdzanie? t/n: ")
+            if go_on == "t":
+                os.system("cls")
+            else:
                 break
 
-        go_on = input("Czy chcesz kontynuowac sprawdzanie? t/n: ")
+        go_on = input("Czy chcesz wybrac inne panstwo? t/n: ")
         if go_on == "t":
             os.system("cls")
         else:
             break
-
 
 main()
